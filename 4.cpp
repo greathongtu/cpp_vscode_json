70f7a7414a0031d953c85a6c97f290734c84c89b
xtrod666 <nicolasshuai@gmail.com>
2023-03-27 15:40:01 +0800

    first commit

---
+++ 4.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <climits>
+#include <iostream>
+#include <iterator>
+#include <numeric>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+
+int main() { 
+  cout << "hello\n";
+  return 0; 
+}
